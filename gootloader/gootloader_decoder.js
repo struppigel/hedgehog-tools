facd152767649ca6e7b4c29f3f9773dc21153f8e
Karsten Hahn <4398909+struppigel@users.noreply.github.com>
2023-12-13 12:23:39 +0100

    Update gootloader_decoder.js

--- gootloader/gootloader_decoder.js
+++ gootloader/gootloader_decoder.js
@@ -24,7 +24,7 @@ const beautifyOpts = {
  * 445a5c6763877994206d2b692214bb4fba04f40a07ccbd28e0422cb1c21ac95b --> complete, has 6 layers
  * cbd826f59f1041065890cfe71f046e59ae0482364f1aaf79e5242de2246fb54b --> complete, has 6 layers
  * b34bcf097ad6ab0459bc6a4a8f487ca3526b6069ec01e8088fd4b00a15420554 --> complete, has 6 layers
- * 1b8b2fbdff9e4109edae317c4dd8cef7bb7877d656e97a3dd0a1e8c0c9d72b0b --> only unpacks until layer 6
+ * 1b8b2fbdff9e4109edae317c4dd8cef7bb7877d656e97a3dd0a1e8c0c9d72b0b --> complete, has 6 layers
  */
 
 if (require.main === module) {
