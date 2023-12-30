d3e5142aa0ae5374d91d8770acd597074c750540
struppigel <struppigel@googlemail.com>
2023-12-30 09:31:41 +0100

    added more output options (-q -c2 -s)

--- Qakbot/qbot_extractor.py
+++ Qakbot/qbot_extractor.py
@@ -1,9 +1,11 @@
 import pefile
 import os
+import sys
 import hashlib
 import re
 import functools
 import struct
+import argparse
 from collections import namedtuple
 
 # samples used: 
@@ -11,14 +13,21 @@ from collections import namedtuple
 # 670e990631c0b98ccdd7701c2136f0cb8863a308b07abd0d64480c8a2412bde4
 # 84669a2a67b9dda566a1d8667b1d40f1ea2e65f06aa80afb6581ca86d56981e7
 
+# global settings
+print_only_c2s = False
+quiet = False
+
+#  verify if the hash of the data is the same as the expected hash
 def verify_hash(data: bytes, expected_hash: bytes):
    return sha1sum(data) == expected_hash
 
+# returns the sha1 hash of a byte list
 def sha1sum(data):
     h  = hashlib.sha1()
     h.update(data)
     return h.digest()
 
+# returns the file's sha256 hash
 def sha256sum(filename):
     h  = hashlib.sha256()
     b  = bytearray(128*1024)
@@ -28,6 +37,7 @@ def sha256sum(filename):
             h.update(mv[:n])
     return h.hexdigest()
 
+# decrypt qbot strings
 def decipher_strings(data: bytes, key: bytes):
    result = []
    current_string = list()
@@ -38,6 +48,7 @@ def decipher_strings(data: bytes, key: bytes):
               current_string = list()
    return result
 
+# code by https://github.com/OALabs/Lab-Notes/blob/main/Qakbot/qakbot.ipynb
 def tohex(data):
     import binascii
     if type(data) == str:
@@ -45,10 +56,12 @@ def tohex(data):
     else:
         return binascii.hexlify(data)
 
+# append config to the config output file
 def write_config_to_file(config, out_file):
     with open(out_file, 'a', encoding='utf-8') as f:
         f.write("------------------------------------------\n\n" + config + "\n")
 
+# extract info that is necessary for decryption, namely: key VA, data VA and data size
 def extract_decryption_data(afile):
     DecryptInfo = namedtuple("DecryptInfo", "key data size")
     decrypt_info_list = []
@@ -70,12 +83,13 @@ def extract_decryption_data(afile):
             decrypt_info_list.append(DecryptInfo(key_addr, data_addr, data_size))
     return decrypt_info_list
 
-# read the data based on VA(!) for the data_addr
+# read the data based on VA for the data_addr
 def read_data(afile, data_addr, data_size):
     data = []
     with open(afile, 'rb') as f:
         pe = pefile.PE(data=f.read())
-        start = data_addr-pe.OPTIONAL_HEADER.ImageBase
+        # it is a VA, so we need to substract image base
+        start = data_addr-pe.OPTIONAL_HEADER.ImageBase 
         data = pe.get_data(start, data_size)
     return data
 
@@ -94,7 +108,7 @@ def extract_str_table_key(afile, key_addr):
             break
         else: prev_was_zero = False
         curr_off += 1
-    print("key length found", end_offset)
+    write("key length found " + str(end_offset))
     return data[:end_offset]
 
 # returns the table data as byte list
@@ -121,6 +135,7 @@ def get_resources(afile):
             resource_data.append(pe.get_memory_mapped_image()[data_rva:data_rva+size])
     return resource_data
 
+# code by https://github.com/OALabs/Lab-Notes/blob/main/Qakbot/qakbot.ipynb
 def rc4crypt(data, key):
     #If the input is a string convert to byte arrays
     if type(data) == str:
@@ -142,27 +157,66 @@ def rc4crypt(data, key):
         out.append(c ^ box[(box[x] + box[y]) % 256])
     return bytes(out)
 
-# return potential keys for configuration decryption
+# prints one decrypted string table alongside the offsets
+# the output format can be used as Python dict
+# result looks like this:
+# 348 : '\\\\.\\pipe\\\x00',
+# 2148 : 'fshoster32.exe\x00',
+# 2978 : 'wmic process call create \'expand "%S" "%S"\'\n\x00
+# ...
+def print_string_lookup(str_list):
+    # this is used to count the offsets properly, using the escaped version would result in wrong offsets
+    strings = [b.decode('latin1') for b in str_list]
+    # looks weird but will put the output string into a format that can be used as string in Python code
+    # even if there are multiline strings or quotes in the decrypted strings
+    strings_escaped = [b.decode('latin1').encode('unicode_escape').decode('latin1').replace("'","\\'") for b in str_list]
+    curr_idx = 0
+    write("\n--------String Table Start --------")
+    for idx, s in enumerate(strings):
+        print(curr_idx, ":", "'"+strings_escaped[idx]+"',")
+        curr_idx += len(s)
+    write("--------String Table End --------\n")
+
+# extracts and decrypts QBots string tables and prints it
+# the output is useful for deobfuscation, it can be put into IDAPython or Binary Ninja API scripts
+# to add comments with the translated strings
+def main_string_lookup(afile):
+    decrypt_info = extract_decryption_data(afile)
+    print()
+    for table_num, info in enumerate(decrypt_info):
+        write("===========Table " + str(table_num + 1) + " ===========\n")
+        write("string key va: " + hex(info.key))
+        write("string table va: " + hex(info.data))
+        write("table size: " + hex(info.size))
+        
+        str_key = extract_str_table_key(afile, info.key)
+        strtable = extract_str_table_data(afile, info.data, info.size)
+        strings_decrypted = decipher_strings(strtable, str_key)
+        print_string_lookup(strings_decrypted)
+
+# returns potential keys for configuration decryption
+# the potential keys are determined by decrypting strings in the string table
 def collect_config_keys(afile):
     decrypt_info = extract_decryption_data(afile)
     strings_bytes = set()
-    for i in decrypt_info:
-        print("======================")
-        print("string key va:", hex(i.key))
-        print("string table va:", hex(i.data))
-        print("table size:", hex(i.size))
+    for table_num, info in enumerate(decrypt_info):
+        write("===========Table " + str(table_num + 1) + " ===========\n")
+        write("string key va: " + hex(info.key))
+        write("string table va: " + hex(info.data))
+        write("table size: " + hex(info.size))
         
-        str_key = extract_str_table_key(afile, i.key)
-        strtable = extract_str_table_data(afile, i.data, i.size)
+        str_key = extract_str_table_key(afile, info.key)
+        strtable = extract_str_table_data(afile, info.data, info.size)
         strings_decrypted = decipher_strings(strtable, str_key)
-        print("decrypted strings from table", len(strings_decrypted), "\n")
+        write("decrypted strings from table " + str(len(strings_decrypted)) + "\n")
         strings_bytes.update(strings_decrypted)
-    print("unique decrypted strings", len(strings_bytes))
+    write("unique decrypted strings " + str(len(strings_bytes)))
     strings_list = [r.decode('latin1') for r in strings_bytes]
     #print("\n".join(strings_list))
     return list(strings_bytes)
 
-# code partially by https://github.com/OALabs/Lab-Notes/blob/main/Qakbot/qakbot.ipynb
+# code by https://github.com/OALabs/Lab-Notes/blob/main/Qakbot/qakbot.ipynb
+# converts the ip table of QBot to a list with IP addresses as strings
 def extract_ips(data):
     ips = []
     ip_table = data[1:]
@@ -177,6 +231,7 @@ def extract_ips(data):
     return ips
 
 # see https://n1ght-w0lf.github.io/malware%20analysis/qbot-banking-trojan/
+# translates QBot configuration keys to their meaning
 def get_config_key_description(key):
     keys = {
         "10" : "campaign",
@@ -194,9 +249,11 @@ def get_config_key_description(key):
     if key in keys: return keys[key]
     else: return key
 
+# converts a dictionary to a string
 def dict_to_string(dictionary):
     return "\n".join([k + ": " + v for k, v in dictionary.items()])
 
+# puts the plaintext configuration into a dictionary, keys are translated by their meaning
 def decode_config(data):
     delim = "="
     try:
@@ -216,6 +273,8 @@ def decode_config(data):
         return None
     return None
 
+# extracts and returns the configuration of QBot as a string
+# to do so it will bruteforce the key, using all decrypted strings from QBots encrypted string table
 def extract_config(afile):
     resources = get_resources(afile)
     decrypted_blobs = []
@@ -231,43 +290,93 @@ def extract_config(afile):
             data = result[20:]
             expected_hash = result[:20]
             if verify_hash(data, expected_hash):
-                print()
-                print("config hash", tohex(expected_hash))
-                print("config key", key_string.decode('latin1'),"\n")
+                write("")
+                write("config hash " + str(tohex(expected_hash)))
+                write("config key " + key_string.decode('latin1') + "\n")
                 decrypted_blobs.append(data)
-    print("successfully decrypted data blobs", len(decrypted_blobs))
+    write("successfully decrypted data blobs " + str(len(decrypted_blobs)))
     smallest_blob = functools.reduce(lambda a, b: a if len(a) < len(b) else b, decrypted_blobs)
     biggest_blob = functools.reduce(lambda a, b: a if len(a) > len(b) else b, decrypted_blobs)
     ips = extract_ips(biggest_blob)
-    print("obtained C2s", len(ips))
+    write("obtained C2s " + str(len(ips)))
     config_data = smallest_blob
     config = decode_config(config_data)
     config_str = ""
     if config:
         config_str += dict_to_string(config) + "\n"
-    else: print("given data blob was not a config")
+    else: write("given data blob was not a config")
     config_str += "ips:" + ", ".join(ips)
     return config_str
 
-def main(folder):
+# -f option
+def main_file(afile):
+    write('checking ' + afile)
+    config = extract_config(afile)
+    config_str = sha256sum(afile) + "\n\n" + config + "\n\n"
+    if not quiet: print("\n" + config)
+    if write_c2s_to_file:
+        write_config_to_file(config_str, c2_output_file)
+    return config_str
+
+# -d option
+def main_folder(folder):
     if folder.endswith('\\'): 
         folder = folder[:-1]
 
     configs = []
     for directory, subdirs, files in os.walk(folder):
         for pfile in files:
-            print('checking', pfile)
             afile = os.path.join(directory, pfile)
-            config = extract_config(afile)
-            config_str = sha256sum(afile) + "\n\n" + config + "\n\n"
+            config_str = main_file(afile)
             configs.append(config_str)
-            write_config_to_file(config_str, 'qbot_configs.txt')
-    print('\nconfigs extracted: ' + str(len(configs)))
+    write('\nconfigs extracted: ' + str(len(configs)))
+
+# print only if no --quiet or --c2 settings were used
+def write(string):
+    if not quiet and not print_only_c2s: print(string)
+
+def main():
+    global print_only_c2s
+    global quiet 
+    global write_c2s_to_file
+    global c2_output_file
+
+    parser = argparse.ArgumentParser(description='QBot config extractor and string decrypter')
+    parser.add_argument('-f', '--file', help='path is a single qbot sample', action='store_true')
+    parser.add_argument('-d', '--directory', help='given path is a directory with samples', action='store_true')
+    parser.add_argument('-o', '--output', help='write C2s to file', action='store', type=str)
+    parser.add_argument('-c2', '--c2', help='print only C2 data, nothing more', action='store_true', default=print_only_c2s)
+    parser.add_argument('-q', '--quiet', help='do not print anything', action='store_true', default=quiet)
+    parser.add_argument('-s', '--strings', help='print decoded strings and their offsets (can be used as dict to deobfuscate with IDAPython or Binary Ninja API)', action='store_true')
+    parser.add_argument('path', help='file or folder to decode')
+
+    args = parser.parse_args()
+    
+    print_only_c2s = args.c2
+    quiet = args.quiet
+    write_c2s_to_file = False
+    
+    if args.output: 
+        write_c2s_to_file = True
+        c2_output_file = args.output
+
+    if args.strings:
+        if args.directory:
+            print("--directory not supported for strings", file=sys.stderr)
+        elif os.path.isfile(args.path):
+            main_string_lookup(args.path)
+        else:
+            print("not a file", file=sys.stderr)
+    elif args.directory:
+        if os.path.isdir(args.path):
+            main_folder(args.path)
+        else:
+            print("not a directory", file=sys.stderr)
+    elif args.file:
+        if os.path.isfile(args.path):
+            main_file(args.path)
+        else:
+            print("not a file", file=sys.stderr)
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) == 0:
-        print("please provide a folder name")
-    else:
-        main(sys.argv[1])
-
+    main()
