3584de58762d5d126ae0785d5a69fc047919a178
Karsten Hahn <4398909+struppigel@users.noreply.github.com>
2024-06-14 12:22:20 +0200

    Update badspace_idapython_string_decrypter.py
    finds decrypt function automatically

--- BadSpace/badspace_idapython_string_decrypter.py
+++ BadSpace/badspace_idapython_string_decrypter.py
@@ -1,13 +1,16 @@
 import idaapi, idc, idautils
 
-# This script is a modified version of Revil String decrypter by OALabs: https://gist.github.com/OALabs/04ef6b2d6203d162c5b3b0eefd49530c
-#
-# It decrypts the strings of BadSpace, renames the labels and applies comments with the decrypted strings to both, disassembly and decompilation.
-# Currently it works only for one sample because it needs the decrypt function address.
+# This script decrypts the strings of BadSpace and applies comments with the decrypted strings to decompilation.
+# Requirements: hexrays decompiler available, encrypted strings are in ".data" or ".rdata" section
 #
 # sample: 6a195e6111c9a4b8c874d51937b53cd5b4b78efc32f7bb255012d05087586d8f
 # author: Karsten Hahn @ GDATA CyberDefense AG
 
+import idautils
+import ida_hexrays
+import sys    
+
+# function from  https://gist.github.com/OALabs/04ef6b2d6203d162c5b3b0eefd49530c
 def rc4crypt(key, data):
     x = 0
     box = list(range(256))
@@ -23,8 +26,22 @@ def rc4crypt(key, data):
         box[x], box[y] = box[y], box[x]
         out.append(chr(char ^ box[(box[x] + box[y]) % 256]))
     return ''.join(out)
+    
+def decrypt_string(string_blob):
+    MAX_LEN = 300
+    key_start = 4
+    key_size = 4
+    str_start = 8
+    str_len = int.from_bytes(idc.get_bytes(string_blob, 4), byteorder='little')
+    if str_len >= MAX_LEN: return None
+    key_data = idc.get_bytes(string_blob + key_start, key_size)
+    str_data = idc.get_bytes(string_blob + str_start, str_len)
+    if key_data == None or str_data == None: return None
+    plaintxt_str =  rc4crypt(key_data, str_data)
+    out_str = plaintxt_str.replace('\x00','')
+    return out_str
 
-# from https://github.com/X-Junior/Malware-IDAPython-Scripts/blob/main/Badspace/badspace.py
+# function from https://github.com/X-Junior/Malware-IDAPython-Scripts/blob/main/Badspace/badspace.py
 def set_comment(address, comment):
     cfunc = idaapi.decompile(address)
     eamap = cfunc.get_eamap()
@@ -45,46 +62,47 @@ def set_comment(address, comment):
             break
         cfunc.del_orphan_cmts()
 
-def find_string_ref(ptr_addr):
-	# looking for instructions of the form: lea rcx, <imm-value>
-    reg_name = 'rcx'
-	regsize = 8
-    inst_name = 'lea'
-    e_count = 0
-    ## count back 500 heads
-    while e_count < 500:
-        e_count += 1
-        ptr_addr = idc.prev_head(ptr_addr)
-        if idc.print_insn_mnem(ptr_addr) == inst_name:
-            tmp_reg_name = idaapi.get_reg_name(idc.get_operand_value(ptr_addr, 0), regsize)
-            if reg_name.lower() == tmp_reg_name.lower():
-                if idc.get_operand_type(ptr_addr, 1) == 2:
-                    return idc.get_operand_value(ptr_addr, 1)
-    return None
+# finds potential decrypt calls and decrypt
+def find_call_and_decrypt(cfunc):
+    class my_call_visitor(ida_hexrays.ctree_visitor_t):
+        
+        def __init__(self):
+            ida_hexrays.ctree_visitor_t.__init__(self, ida_hexrays.CV_FAST) # CV_FAST does not keep parents nodes in CTREE
+        
+        def visit_expr(self, e):
+	        # only interested in calls
+            if e.op != ida_hexrays.cot_call:
+                return 0
+                
+            called = e.x 
+            if ( called.op != ida_hexrays.cot_obj # called function has right node type
+                or e.a.size() != 1 ): # number of arguments is 1
+                return 0
+            
+            arg1 = e.a.at(0) # obtain first argument
+            # a is the argument vector for the function call
+            if arg1 and arg1.op == ida_hexrays.cot_ref: # make sure it is a reference
+                encrypted_data_addr = arg1.x.obj_ea
+                segment = get_segm_name(encrypted_data_addr)
+                # make sure target is in correct segment
+                if segment != ".data" and segment != ".rdata": return 0 
+                decrypted = decrypt_string(encrypted_data_addr)
+                if decrypted:
+                    print("call arg found at 0x%x with data addr 0x%x" % (arg1.ea, encrypted_data_addr))
+                    print("decrypted", decrypted)
+                    set_comment(arg1.ea, decrypted)
+                    idc.set_name(encrypted_data_addr, decrypted, idaapi.SN_FORCE)
+            return 0 # continue traversal
+    v = my_call_visitor()
+    v.apply_to(cfunc.body, None)
 
-def get_xref_list(fn_addr):
-    return [addr.frm for addr in idautils.XrefsTo(fn_addr)]
+        
+if not idaapi.init_hexrays_plugin():   
+    sys.exit()     
     
-def decrypt_string(fn_address):
-    key_start = 4
-    key_size = 4
-    str_start = 8
-    string_blob = find_string_ref(fn_address)
-    if string_blob == None: return
-    str_len = int.from_bytes(idc.get_bytes(string_blob, 4), byteorder='little')
-    key_data = idc.get_bytes(string_blob + key_start, key_size)
-    str_data = idc.get_bytes(string_blob + str_start, str_len)
-    plaintxt_str =  rc4crypt(key_data, str_data)
-    out_str = plaintxt_str.replace('\x00','')
-    print("0x%x: %s" % (fn_address, out_str))
-    set_comment(fn_address, out_str)
-    # set label for data address
-    idc.set_name(string_blob, out_str, idaapi.SN_FORCE)
-
-def decrypt_all_strings(fn_address):
-    for ptr in get_xref_list(fn_address):
-        decrypt_string(ptr)
-
-# TODO automate function search
-decrypt_all_strings(0x2EDA35E40)
-decrypt_all_strings(0x2EDA35DC0)
+# iterate all functions in all segments
+for segea in idautils.Segments():
+    for func_ea in idautils.Functions(segea, get_segm_end(segea)):
+        cfunc = idaapi.decompile(func_ea)
+        find_call_and_decrypt(cfunc)
+print("done")
