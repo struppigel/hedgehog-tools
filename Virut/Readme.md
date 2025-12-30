23f58f28b8bb369c10dea352f8bd696301d9392b
struppigel <struppigel@googlemail.com>
2025-12-30 16:55:42 +0100

    added the assembly code for generating Virut bait files

--- Virut/Readme.md
+++ Virut/Readme.md
@@ -1,5 +1,12 @@
-## Virut API Resolver
+## Virut analysis scripts
+
+
+### Ghidra_Virut_ResolveApiHashes.py and python3_virut_resolve_api_hashes.py
 
 Resolves API hashes of Virut sample:  https://malshare.com/sample.php?action=detail&hash=096607aa89ea6f17e5a815a67b94bc245ecbf18a87705e1dec2f1d85f8350e32
 
 Watch me analyse this sample here: https://youtu.be/250Bxe0qlQY
+
+### zerobytes.asm and Makefile
+
+Generate a bait file that Virut wants to infect.
---
+++ Virut/zerobytes.asm
@@ -0,0 +1,28 @@
+; zerobytes.asm
+; Compile: yasm -f win32 zerobytes.asm -o zerobytes.obj
+; Link:    link.exe /subsystem:windows /entry:main zerobytes.obj user32.lib kernel32.lib /out:zerobytes.exe
+
+extern _MessageBoxA@16
+extern _ExitProcess@4
+
+section .data
+    msg_title db "Hello", 0
+    msg_text  db "Hello World!", 0
+    times 1024 db 0               ; 1 KB of zero padding in .data
+
+section .text
+global main
+main:
+    push 0                        ; MB_OK
+    push msg_title                ; lpCaption
+    push msg_text                 ; lpText
+    push 0                        ; hWnd = NULL
+    call _MessageBoxA@16
+
+    push 0
+    call _ExitProcess@4
+
+    times 4096 db 0               ; 2 KB zero padding in .text
+
+section '.rsrc' data align=4
+    times 65536 db 0               ; 1 KB zero-filled .rsrc section
